dc004ba1c31b9d42dcd68d85ade7d3b45f4769a3
blancaamaldonadoo <159050395+blancaamaldonadoo@users.noreply.github.com>
2024-03-13 10:01:37 +0000

    cambios

---
+++ demo/src/main/java/com/example/Experimento.java
@@ -0,0 +1,6 @@
+
+public class Experimento extends Poblacion{
+
+    
+
+}
---
+++ demo/src/main/java/com/example/Poblacion.java
@@ -0,0 +1,27 @@
+import java.sql.Date;
+
+public class Poblacion{
+
+    String nombre;
+    Date fechaInicio;
+    Date fechaFin;
+    int numBacterias;
+    double temperatura;
+    Dosis dosisComida;
+
+    public Poblacion(String nombre, Date fechaInicio, Date fechaFin, int numBacterias, double temperatura, Dosis dosisComida){
+        this.nombre = nombre;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.numBacterias = numBacterias;
+        this.temperatura = temperatura;
+        this.dosisComida = dosisComida;
+    }
+
+    //Métodos:
+
+
+    //toString:
+    
+
+}
